446966452c6b5c0a76b900a07c392aa9d99d395e
Taisiia <taisiia.pomazan@gmail.com>
2020-03-18 12:44:11 +0300

    finally

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
-
-// loads the Icon plugin
 UIkit.use(Icons);
-
-// components can be called from the imported UIkit reference
 UIkit.notification('Webpack UIKit 3 bundle loaded!');
+
+document.getElementById('close-modal').addEventListener('click', closeModalWindow);
+document.getElementById('hide-balance').addEventListener('click', hideBalance);
+
+function closeModalWindow () {
+  console.log('jhjhg');
+  document.getElementById('modal-center').style.opacity = "0";
+}
+
+function hideBalance () {
+  document.getElementById('modal-center').style.opacity = "0";
+  document.querySelector('.header__card-balance--visible').style.display = "none";
+  document.querySelector('.header__card-balance--invisible').style.display = "inline";
+}
